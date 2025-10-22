4de0474514939e06145eab5ef53f948517cfc7cd
Danesh <techydanesh@gmail.com>
2025-10-22 11:32:03 +0530

    google auth credentials added

--- backend/src/config/environments/config.js
+++ backend/src/config/environments/config.js
@@ -6,4 +6,6 @@ module.exports = {
   port: process.env.PORT,
   dbUrl: process.env.DB_URL,
   jwtSecret: process.env.JWT_SECRET,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 };
